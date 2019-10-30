f53fe8e44c5efe88dafef5a05daf603bb9453064
Chase Lipscomb <chaselipscomb97@gmail.com>
2019-10-30 14:18:30 -0700

    jawsDB

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+INSERT INTO cats (name) VALUES ('Chicken Burger');
+INSERT INTO cats (name) VALUES ('Tuna Burger');
+INSERT INTO cats (name, sleepy) VALUES ('Steak Burger', true);
+INSERT INTO cats (name, sleepy) VALUES ('Fish Burger', true);
+INSERT INTO cats (name, sleepy) VALUES ('Vegan Burger', true);
+INSERT INTO cats (name) VALUES ('Vegetarian Burger');
